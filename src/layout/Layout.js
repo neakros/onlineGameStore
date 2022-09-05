10c1d1cda417b54fcafa0fb74187334f38d68aaf
neakros <neakros150@gmail.com>
2022-09-05 18:34:32 +0300

    first commit

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,12 +6,14 @@ import {useSelector} from "react-redux";
 
 const Layout = () => {
     const auth = useSelector(state => state.auth)
+    const game = useSelector((state => state.basket.basketStore))
+    const totalPrise = game.reduce((acc, game) => acc += game.price, 0)
     return (
         <>
             <header className="d-flex justify-content-between mt-2">
                 <NavLink to='/'><GiGameConsole fontSize={30}/> Game store</NavLink>
                 <div className="d-flex gap-3">
-                    <NavLink to='/basket'><AiOutlineShoppingCart fontSize={25}/> 0 грн.</NavLink>
+                    <NavLink to='/basket'><AiOutlineShoppingCart fontSize={25}/> {totalPrise} грн.</NavLink>
                     {/*{!auth ?  <NavLink to='/basket'><AiOutlineShoppingCart fontSize={25}/></NavLink>*/}
                     {/*    :  <NavLink to='/basket'><AiOutlineShoppingCart fontSize={25}/></NavLink>}*/}
                     {auth ?  <NavLink to='/adminPanel'>adminPanel</NavLink> : ''}
